c2adac7586c9654ee2ac999156b96367ef0bcfd9
Donald Lobree <edl1155@gmail.com>
2019-11-23 18:58:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,12 +41,14 @@ end
   cart
 end
 
-def apply_clearance(cart)
-  # code here
-  cart.each do |product_name, stats|
-    stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
-  end
-  cart
+def apply_clearance(cart) 
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
 end
 
 def checkout(cart, coupons)
